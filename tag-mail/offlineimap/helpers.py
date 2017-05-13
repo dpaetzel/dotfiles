8005de09f02ad6c8d28787ae9b4f59f2a97c898b
David Pätzel <david.a.paetzel+github@gmail.com>
2017-05-13 14:04:15 +0200

    Incorporate new mail account

--- tag-mail/offlineimap/helpers.py
+++ tag-mail/offlineimap/helpers.py
@@ -30,23 +30,6 @@ def exclude(excludes):
 
     return inner
 
-# yandex_mapping = { 'INBOX'                       : 'Inbox'
-#           , '&BCcENQRABD0EPgQyBDgEOgQ4-'         : 'Drafts'
-#           , '&BB4EQgQ,BEAEMAQyBDsENQQ9BD0ESwQ1-' : 'Sent'
-#           , '&BCEEPwQwBDw-'                      : 'Spam'
-#           , '&BCMENAQwBDsENQQ9BD0ESwQ1-'         : 'Trash'
-# # not fully sure about starred
-#           , '&BBgEQQRFBD4ENARPBEkEOAQ1-'         : 'Starred'
-#           }
-
-# r_yandex_mapping = { val: key for key, val in yandex_mapping.items() }
-
-# def yandex_remote_nt(folder):
-#     return yandex_mapping.get(folder, folder)
-
-# def yandex_local_nt(folder):
-#     return r_yandex_mapping.get(folder, folder)
-
 def client_id(account):
   cmd = ["cat", "/home/david/.offlineimap/%s/client_id" % account]
   try:
@@ -67,3 +50,27 @@ def refresh_token(account):
       return subprocess.check_output(cmd).strip()
   except subprocess.CalledProcessError:
       return ""
+
+def get_pass(address):
+  args = ["pass", "mail/%s" % address]
+  try:
+    return subprocess.check_output(args).strip()
+  except subprocess.CalledProcessError:
+    return ""
+
+# yandex_mapping = { 'INBOX'                       : 'Inbox'
+#           , '&BCcENQRABD0EPgQyBDgEOgQ4-'         : 'Drafts'
+#           , '&BB4EQgQ,BEAEMAQyBDsENQQ9BD0ESwQ1-' : 'Sent'
+#           , '&BCEEPwQwBDw-'                      : 'Spam'
+#           , '&BCMENAQwBDsENQQ9BD0ESwQ1-'         : 'Trash'
+# # not fully sure about starred
+#           , '&BBgEQQRFBD4ENARPBEkEOAQ1-'         : 'Starred'
+#           }
+
+# r_yandex_mapping = { val: key for key, val in yandex_mapping.items() }
+
+# def yandex_remote_nt(folder):
+#     return yandex_mapping.get(folder, folder)
+
+# def yandex_local_nt(folder):
+#     return r_yandex_mapping.get(folder, folder)
