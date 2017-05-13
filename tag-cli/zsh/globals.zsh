9a6729e99e2c36e3c7517956b0b4eaff813e690f
David Pätzel <david.a.paetzel+github@gmail.com>
2017-05-13 13:39:30 +0200

    Add some zsh aliases

--- tag-cli/zsh/globals.zsh
+++ tag-cli/zsh/globals.zsh
@@ -1,11 +1,22 @@
 # global aliases
 alias -g B='&!'
+
 alias -g H='| head'
+alias -g F="| head -$(($(tput lines) - 3))" # “full screen”
 alias -g T='| tail'
 alias -g G='| grep --color -i -E'
 alias -g S='| sed -r'
 alias -g L='| less'
 alias -g W='| wc -l'
+
+alias -g HH='|& head'
+alias -g FF="|& head -$(($(tput lines) - 3))" # “full screen”
+alias -g TT='|& tail'
+alias -g GG='|& grep --color -i -E'
+alias -g SS='|& sed -r'
+alias -g LL='|& less'
+alias -g WW='|& wc -l'
+
 alias -g NF='*(.om[1])'
 alias -g NFF='*(/om[1])'
 alias -g ND='~/Temporary/*(.om[1])'
