7747d8485c741e3cc0b742b87f69195b0247694c
David Pätzel <david.paetzel@posteo.de>
2020-02-21 16:42:54 +0100

    Add initial doom-emacs config

---
+++ tag-emacs/doom.d/config.el
@@ -0,0 +1,104 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "David Pätzel"
+      user-mail-address "david.paetzel@posteo.de")
+
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font "Inconsolata-14")
+
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. These are the defaults.
+(setq doom-theme 'doom-one)
+
+
+;; If you want to change the style of line numbers, change this to `relative' or
+;; `nil' to disable it:
+(setq display-line-numbers-type 'relative)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', where Emacs
+;;   looks when you load packages with `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
+
+
+(setq doom-localleader-key ",")
+
+
+(map!
+ :leader "w/" 'evil-window-vsplit
+ :leader "w-" 'evil-window-split
+ :leader :desc "Save all open files" "fS" 'evil-write-all)
+
+
+(setq-default
+   whitespace-style '(face trailing tabs))
+
+
+(add-hook 'focus-out-hook 'evil-write-all)
+
+
+;; package-specific settings
+
+
+(use-package! avy)
+(map! :leader "j" 'evil-avy-goto-char :after avy)
+
+
+;; evil-surround
+(map! :map evil-surround-mode-map :v "s" 'evil-embrace-evil-surround-region)
+(setq-default
+ evil-surround-pairs-alist
+ (append
+  evil-surround-pairs-alist
+  '((?„ . ("„" . "“"))
+    (?“ . ("“" . "”"))
+    (?‚ . ("‚" . "‘"))
+    (?‘ . ("‘" . "’"))))
+ ;; in order for evil-surround to work, I also have to instruct evil-embrace to
+ ;; pass these keys through
+ evil-embrace-evil-surround-keys
+ (append
+  evil-embrace-evil-surround-keys
+  '(?„
+    ?“
+    ?‚
+    ?‘)))
+
+
+;; mode-specific settings
+
+
+(map!
+ :map ledger-mode-map :localleader "F"  'ledger-mode-clean-buffer :after ledger)
+
+
+(load! "latex-config.el")
+
+
+(load! "org-config.el")
---
+++ tag-emacs/doom.d/org-config.el
@@ -0,0 +1,160 @@
+;;; ~/.doom.d/org-config.el -*- lexical-binding: t; -*-
+
+
+(setq-default
+ org-agenda-files
+ (cons "~/TODO.org" (append
+                     (mapcar 'abbreviate-file-name
+                             (split-string
+                              (shell-command-to-string "find ~/Projekte -name \"*.org\"")
+                              "\n"))
+                     (mapcar 'abbreviate-file-name
+                             (split-string
+                              (shell-command-to-string "find ~/Literatur -name \"*.org\"")
+                              "\n")))))
+
+
+(map! :map org-mode-map
+      :localleader "*" 'org-ctrl-c-star :after org
+      :localleader "-" 'org-ctrl-c-minus :after org
+      :n "C-RET" 'org-insert-heading :after org
+      :n "C-S-RET" 'org-insert-todo-heading :after org)
+
+
+(after! org
+
+  (setq-default
+
+   org-startup-indented nil
+   org-agenda-skip-deadline-prewarning-if-scheduled 2
+   org-agenda-skip-scheduled-if-done t
+   org-agenda-tags-column -90
+   org-bullets-bullet-list '("*")
+   org-catch-invisible-edits 'error
+   org-hide-leading-stars t
+   org-list-allow-alphabetical t
+   org-log-into-drawer t
+   org-pretty-entities t
+
+   org-stuck-projects
+   '("+TODO=\"PROJECT\"" ("NEXT" "WAIT" "1" "2" "3") nil "OK")
+
+   org-todo-keyword-faces
+   '(("TODO" :foreground "dim gray" :weight bold)
+     ("WAIT" :foreground "dim gray" :weight bold)
+     ("THEN" :foreground "dim gray" :weight bold)
+     ("MAYBE" :foreground "dim gray" :weight bold))
+
+   org-use-property-inheritance t
+
+   org-file-apps
+   '((auto-mode . emacs)
+     ("\\.mm\\'" . default)
+     ("\\.x?html?\\'" . default)
+     ("\\.pdf\\'" . "zathura %s"))
+
+   org-stuck-projects
+   '("+TODO=\"PROJECT\""
+     ("NEXT" "WAIT" "1" "2" "3")
+     ("OK")
+     ":OK:")
+
+   org-tag-alist
+   '((:startgroup)
+     ("Arbeit" . 97)
+     ("daheim" . 100)
+     (:endgroup))
+
+   org-todo-keywords
+   '((sequence "NEXT(n)" "TODO(t)" "PROJECT(p)" "WAIT(w)" "THEN(h)" "MAYBE(m)" "1(1)" "2(2)" "3(3)" "DONE(d!)")))
+
+  (setq!
+
+   org-agenda-skip-function-global
+   '(org-agenda-skip-entry-if
+     'todo
+     '("THEN"))
+
+   org-agenda-start-day "+0d"
+
+   org-agenda-custom-commands
+   '(("R" "wöchentliches Review"
+      ((todo "WAIT"
+             ((org-agenda-overriding-header "Warten auf")))
+       (todo "NEXT|1|2|3"
+             ((org-agenda-overriding-header "Next Actions")))
+       (todo "TODO"
+             ((org-agenda-overriding-header "andere TODOs (noch nicht als Next Action ausführbares etc.)")))
+       (todo "THEN"
+             ((org-agenda-overriding-header "TODOs, die von anderen abhängen")))
+       (todo "MAYBE"
+             ((org-agenda-overriding-header "Vielleicht/Eines Tages")))
+       (todo "PROJECT"
+             ((org-agenda-overriding-header "Projekte")))
+       (stuck ""
+              ((org-agenda-overriding-header "Projekte ohne NEXT oder WAIT")))
+       (todo "DONE"
+             ((org-agenda-overriding-header "Erledigtes"))))
+      nil  ;; settings
+      nil) ;; files to write agenda to
+
+     ("p" "Projekte (Arbeit)"
+      ((tags-todo "+TODO=\"PROJECT\"-daheim" nil)
+       (tags-todo "+TODO=\"PROJECT\"+daheim" nil))
+      ((org-agenda-start-day "+0d"))
+      nil) ;; files to write agenda to
+
+     ("w" "Mein Workflow"
+      tags
+      "+Promotion"
+      ((org-agenda-start-day "+0d")
+       (org-agenda-skip-function
+        '(org-agenda-skip-entry-if
+          'notscheduled)))
+      nil) ;; files to write agenda to
+
+     ("N" "Alle Next Actions"
+      tags-todo
+      "+TODO=\"NEXT\"-lesen|TODO=\"1\"|TODO=\"2\"|TODO=\"3\""
+      ((org-agenda-start-day "+0d"))
+      nil) ;; files to write agenda to
+
+     ("n" "Alle Next Actions (Arbeit)"
+      tags-todo
+      "+TODO=\"NEXT\"-lesen-daheim|TODO=\"1\"|TODO=\"2\"|TODO=\"3\""
+      ((org-agenda-start-day "+0d"))
+      nil) ;; files to write agenda to
+
+     ("A" "Agenda (Arbeit)"
+      ((agenda ""
+               ((org-agenda-overriding-header "Arbeit")
+                (org-agenda-span
+                 'day)
+                (org-agenda-skip-function
+                 '(org-agenda-skip-subtree-if
+                   'regexp
+                   ":daheim:"))))
+       (agenda ""
+               ((org-agenda-overriding-header "daheim")
+                (org-agenda-span
+                 'day)
+                (org-agenda-skip-function
+                 '(org-agenda-skip-subtree-if
+                   'notregexp
+                   ":daheim:")))))
+      ;; nil
+      ((org-agenda-start-day "+0d"))
+      nil) ;; files to write agenda to
+
+     ("y" "Theory"
+      tags
+      "+theory-notes"
+      ((org-agenda-start-day "+0d"))
+      nil) ;; files to write agenda to
+
+     ("l" "zu lesende Paper"
+      ((todo "1" nil)
+       (todo "2" nil)
+       (todo "3" nil))
+      ((org-agenda-start-day "+0d"))
+      nil)))) ;; files to write agenda to
