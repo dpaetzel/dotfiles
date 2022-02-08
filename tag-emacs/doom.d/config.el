d3ad4e6abb71408fc33c10024b3b28377494afd7
David Pätzel <david.paetzel@posteo.de>
2022-02-08 08:25:49 +0100

    :wrench: Switch to Fira Code (emacs)

--- tag-emacs/doom.d/config.el
+++ tag-emacs/doom.d/config.el
@@ -16,7 +16,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font "Inconsolata-14")
+;; (setq doom-font "Inconsolata-14")
+(setq doom-font (font-spec :family "Fira Code Nerd Font" :size 18 :style "Retina"))
 
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
